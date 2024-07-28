b7ad1f61476c39aea5b55f5ff9d8f32257d62d39
Sergey Mushta <opengeekslabserv@gmail.com>
2024-07-28 12:58:05 +0200

    Chat

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ import cors from 'cors';
 import path from 'path';
 import http from 'http';
 import fs from 'fs';
-import { Server } from 'socket.io';
-import _ from 'lodash';
 
 import { getDirName } from './lib/utils/functions.js';
 import dbClass from './lib/common/DbService.js';
@@ -18,6 +16,9 @@ import authApi from './lib/api/AuthApi.js';
 import adminListApi from './lib/api/AdminListAPI.js';
 import iBenchPanelAction from './lib/api/IBenchPanelAPI.js';
 import backAdminListApi from './lib/api/BackAdminListAPI.js';
+import UsersListAPI from './lib/api/UsersListAPI.js';
+import ChatAPI from './lib/api/ChatAPI.js';
+import { initSocket } from './lib/utils/socket.js';
 
 const __dirname = getDirName(import.meta.url);
 dotenv.config({ path: `${__dirname}/.env` });
@@ -62,6 +63,8 @@ router.use(authApi);
 router.use(adminListApi);
 router.use(iBenchPanelAction);
 router.use(backAdminListApi);
+router.use(UsersListAPI);
+router.use(ChatAPI);
 
 const port = process.env.PORT || 3002;
 //initialize server and add router to it
@@ -76,43 +79,5 @@ const server = http
       : console.log(`server is listening on ${port}`);
   });
 
-const socketIO = new Server(server);
-
-const socketUsers = {};
-
-socketIO.on('connection', (socket) => {
-  console.log('===CONNECT', socket.id);
-
-  socket.on('disconnect', () => {
-    delete socketUsers[socket.id];
-    console.log('===disconnect', socketUsers);
-  });
-
-  socket.on('register', (data) => {
-    const userData = JSON.parse(data);
-    socketUsers[socket.id] = {
-      user: userData,
-      socket,
-    };
-    console.log('===REGISTER SOCKET ID', socket.id);
-  });
-
-  socket.on('test', (data) => {
-    console.log('===CONNECT test', data);
-    socket.emit('back-socket', 'Hello socket');
-  });
-
-  socket.on('send_message', (message) => {
-    const userData = socketUsers[socket.id];
-    console.log('===SEND MESSAGE', message);
-    const messageData = {
-      message,
-      ...userData.user,
-    };
-
-    Object.values(socketUsers).forEach(userData => {
-      userData.socket.emit('get_message', JSON.stringify(messageData));
-    });
-  });
-});
+initSocket(server);
 
